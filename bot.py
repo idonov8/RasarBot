74278ac2c1fed07b7658860c74ca2d4618ddb000
Ido Nov <idonov8@gmail.com>
2020-02-23 18:39:32 +0200

    now kinda works. really early mvp, has a lot of problems

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 import telegram
-from telegram.ext import  Updater, InlineQueryHandler, CommandHandler, Defaults,Job
+from telegram.ext import  Updater, InlineQueryHandler, CommandHandler, Defaults, Job, MessageHandler, Filters
 import requests # to make requests to external api
 import re # regex for pictures of doggos
 import logging
@@ -38,22 +38,42 @@ def bop(bot, update):
     url = get_image_url()
     bot.send_photo(chat_id=chat_id, photo=url)  
 
-def report(bot, update):
-    global SHAGS_REPS
+def welcome(bot, update):
     chat_id = update.message.chat_id
-    shag = update.message.text.split()[1]
-    kind = update.message.text.split()[2]
 
-    custom_keyboard = [['קטן רס"ר', 'קטן נקי'], 
-                       ['גדול רס"ר', 'גדול נקי']]
+    shag, kind = 'קטן', 'נקי'
+    reports_num = SHAGS_REPS[shag + ' ' + kind]
+    bot.send_message(chat_id=chat_id, text= 'שג ' + shag + " " +kind + 
+                                                    ' מס דיווחים: ' + str(reports_num))
+    shag, kind = 'גדול', 'נקי'
+    reports_num = SHAGS_REPS[shag + ' ' + kind]
+    bot.send_message(chat_id=chat_id, text= 'שג ' + shag + " " +kind + 
+                                                    ' מס דיווחים: ' + str(reports_num))
+
+    custom_keyboard = [['/report גדול נקי', '/report קטן נקי'], 
+                       ['/report גדול רס"ר', '/report קטן רס"ר']]
     reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard)
     bot.send_message(chat_id=chat_id, 
                  text="בחרו מהאפשרויות לדיווח", 
                  reply_markup=reply_markup)
 
+    
+
+def cancel_report(bot, update):
+    #TODO: implement
+    chat_id = update.message.chat_id
+    bot.send_message(chat_id=chat_id, text='דיווח בוטל')
+    
+def report(bot, update):
+    global SHAGS_REPS
+    chat_id = update.message.chat_id
+    shag = update.message.text.split()[1]
+    kind = update.message.text.split()[2]
     SHAGS_REPS[shag + ' ' + kind] += 1
     reports_num = SHAGS_REPS[shag + ' ' + kind]
     logger.info(shag + kind +' reports: ' + str(reports_num))
+    custom_keyboard = ['/cancel_report']
+    reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard)
     bot.send_message(chat_id=chat_id, text='תודה שדיווחת!')
     bot.send_message(chat_id=chat_id, text= 'שג ' + shag + " " +kind + 
                                                     ' מס דיווחים: ' + str(reports_num))
@@ -61,8 +81,10 @@ def report(bot, update):
 def main():
     updater = Updater('1085565057:AAH08Gb5L8yB9rIVdLsbrQKx3yTNM_2PJGA' )
     dp = updater.dispatcher
+    dp.add_handler(CommandHandler('cancel_report',cancel_report))
     dp.add_handler(CommandHandler('bop',bop))
     dp.add_handler(CommandHandler('report',report))
+    dp.add_handler(MessageHandler(Filters.text, welcome))
     updater.start_polling()
     updater.idle()
 
